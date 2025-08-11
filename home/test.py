061a6876f4a042729d93c8589f644c0392bba0c8
py9245 <py9245@naver.com>
2025-08-11 23:20:14 +0900

    3solve

--- home/test.py
+++ home/test.py
@@ -1,88 +1,35 @@
 import sys
 sys.stdin = open('input.txt', 'r')
-
-dxy = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-
-dir_next = {
-    0: [0, 3, 1, 2, 2, 2, 0, 0, 0, 0, 0],
-    1: [1, 2, 3, 3, 0, 3, 1, 1, 1, 1, 1],
-    2: [2, 0, 0, 1, 3, 0, 2, 2, 2, 2, 2],
-    3: [3, 1, 2, 0, 1, 1, 3, 3, 3, 3, 3],
-}
+from collections import deque
 
 T = int(input())
+
 for case in range(1, T + 1):
-    N = int(input().strip())
-    board = [list(map(int, input().split())) for _ in range(N)]
-    worm = [[] for _ in range(5)]
-    for i in range(N):
-        for j in range(N):
-            v = board[i][j] - 6
-            if 0 <= v <= 4:
-                worm[v].append((i, j))
+    N, K = map(int, input().split())
+    answer = set()
+    turn = N // 4
+    string = input()
+    for t in range(turn):
+        for i in range(4):
+            answer.add(int(string[i * turn : (i + 1) * turn], 16))
+        string = string[-1] + string[:-1]
+    print(f"#{case} {sorted(list(answer))[-K]}")
 
-    worm_dict = {}
-    for i in range(5):
-        if worm[i]:
-            worm_dict[worm[i][0]] = worm[i][1]
-            worm_dict[worm[i][1]] = worm[i][0]
-    
-    best = 0
-    visited_dict = {}
-    for i in range(N):
-        for j in range(N):
-            if board[i][j] != 0:
-                continue
-            for d in range(4):
-                if (i == 0 and d == 3) or (i == N - 1 and d == 1):
-                    best = max(1, best)
-                    continue
-                if (j == 0 and d == 0) or (j == N - 1 and d == 2):
-                    best = max(1, best)
-                    continue
-                x, y = i, j
-                total = 0
-                while True:
-                    nx, ny = x + dxy[d][0], y + dxy[d][1]
 
-                    if not (0 <= nx < N and 0 <= ny < N):
-                        d = dir_next[d][5]
-                        total += 1
-                        if visited_dict.get((x, y, d), -1) >= total:
-                            break
-                        visited_dict[(x, y, d)] = total
-                        x += dxy[d][0]
-                        y += dxy[d][1]
-                        if (x, y) == (i, j):
-                            best = max(best, total)
-                            break
-                        continue   
 
-                    v = board[nx][ny]
 
-                    if v == -1 or (nx == i and ny == j):
-                        best = max(total, best)
-                        break
-
-                    if v == 0:
-                        x, y = nx, ny
-                        if visited_dict.get((x, y, d), -1) >= total:
-                            break
-                        visited_dict[(x, y, d)] = total
-                        continue
-
-                    if v > 5:
-                        tx, ty = worm_dict[(nx, ny)]
-                        x, y = tx, ty
-                        if visited_dict.get((x, y, d), -1) >= total:
-                            break
-                        visited_dict[(x, y, d)] = total
-                        continue
-
-                    nd = dir_next[d][v]
-                    x, y, d = nx, ny, nd
-                    total += 1
-                    if visited_dict.get((x, y, d), -1) >= total:
-                        break
-                    visited_dict[(x, y, d)] = total
-    print(f"#{case} {best}")
+#부분회문??
+    # N = int(input())
+    # strings = [input() for _ in range(8)]
+    # answer = 0
+    # for i in range(8):
+    #     for j in range(9 - N):
+    #         row = True
+    #         col = True
+    #         for k in range(N//2):
+    #             if not(row and strings[i][j + k] == strings[i][j + (N - k - 1)]):
+    #                 row = False
+    #             if not(col and strings[j + k][i] == strings[j + (N - k - 1)][i]):
+    #                 col = False
+    #         if row: answer += 1
+    #         if col: answer += 1
