ee33a329b307aa34f700232df808d59a58d92840
py9245 <py9245@naver.com>
2025-08-12 21:20:06 +0900

    등산로

--- 250812/등산로.py
+++ 250812/등산로.py
@@ -1,48 +1,53 @@
 import sys
-from collections import deque
 sys.stdin = open("input.txt", "r")
 
 T = int(input())
 
-dxy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
+dxy = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 for case in range(1, T + 1):
     N, K = map(int, input().split())
-    board = [[0] * N for _ in range(N)]
+    board = [list(map(int, input().split())) for _ in range(N)]
     visited = [[False] * N for _ in range(N)]
-    max_num = 0
-    start_idx = []
-    for i in range(N): # 등산로 보드 초기화, 높은산, 높은산 인덱스 확보
-        for j, v in enumerate(map(int, input().split())):
-            board[i][j] = v
-            if v > max_num:
-                max_num = v
-                start_idx = [(i,j)]
-            elif v == max_num:
-                start_idx.append((i, j))
-    best = [0]
 
-    def dfs(num, x, y, tr, total, vi):
-        if total > best[0]:
-            best[0] = total
-        vi[x][y] = True
+    highest = max(map(max, board))
+    start_idx = [(i, j) for i in range(N) for j in range(N) if board[i][j] == highest]
+    best = 0
+    end_if = False
+
+    def dfs(x, y, tr, total):
+        global best, end_if
+        visited[x][y] = True
+
+        if total > best:
+            best = total
+
+            if best == N * N:
+                end_if = True
+                visited[x][y] = False
+                return
+
+        num = board[x][y]
+
         for dx, dy in dxy:
+            if end_if:
+                return
             nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < N:
-                if vi[nx][ny]:
-                    continue
-                n_num = board[nx][ny]
-                if num > n_num:
-                    nvi = [col[:] for col in vi]
-                    dfs(n_num, nx, ny, True, total + 1, nvi)
-                else:
-                    if tr and n_num - num < K:
-                        nvi = [col[:] for col in vi]
-                        dfs(num - 1, nx, ny, False, total + 1, nvi)
+            if not(0 <= nx < N and 0 <= ny < N) or visited[nx][ny]:
+                continue
+
+            n_num = board[nx][ny]
+
+            if num > n_num:
+                dfs(nx, ny, tr, total + 1)
+            elif n_num - K < num and tr:
+                board[nx][ny] = num - 1
+                dfs(nx, ny, False, total + 1)
+                board[nx][ny] = n_num
+        visited[x][y] = False
 
 
     for i, j in start_idx:
-        dfs(max_num, i, j, True, 1, visited)
+        dfs(i, j, True, 1)
 
-    print(f"#{case} {best[0]}")
+    print(f"#{case} {best}")
