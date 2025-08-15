4bfe46e7ee5b6eabe34812c6287d5281b4febf6e
py9245 <py9245@naver.com>
2025-08-15 13:24:47 +0900

    update

--- 250815/boj1987_알파벳.py
+++ 250815/boj1987_알파벳.py
@@ -1,35 +1,35 @@
-# import sys
-# sys.stdin = open("input.txt", "r")
-# input = sys.stdin.readline
-#
-# dxy = [(0,1),(0,-1),(1,0),(-1,0)]
-#
-# R, C = map(int, input().split())
-# alpha = [input() for _ in range(R)]
-# max_len = len({ch for row in alpha for ch in row})
-#
-# answer = 0
-# end_point = False
-#
-# start_mask = 1 << (ord(alpha[0][0]) - ord('A'))
-# stack = [(0, 0, start_mask, 1)]  # (x, y, visited_mask, total)
-#
-# while stack:
-#     x, y, mask, total = stack.pop()
-#
-#     if total > answer:
-#         answer = total
-#         if total == max_len:
-#             break
-#
-#     for dx, dy in dxy:
-#         nx, ny = x + dx, y + dy
-#         if 0 <= nx < R and 0 <= ny < C:
-#             c_bit = 1 << (ord(alpha[nx][ny]) - ord('A'))
-#             if not (mask & c_bit):
-#                 stack.append((nx, ny, mask | c_bit, total + 1))
-#
-# print(answer)
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+dxy = [(0,1),(0,-1),(1,0),(-1,0)]
+
+R, C = map(int, input().split())
+alpha = [input() for _ in range(R)]
+max_len = len({ch for row in alpha for ch in row})
+
+answer = 0
+end_point = False
+
+start_mask = 1 << (ord(alpha[0][0]) - ord('A'))
+stack = [(0, 0, start_mask, 1)]  # (x, y, visited_mask, total)
+
+while stack:
+    x, y, mask, total = stack.pop()
+
+    if total > answer:
+        answer = total
+        if total == max_len:
+            break
+
+    for dx, dy in dxy:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < R and 0 <= ny < C:
+            c_bit = 1 << (ord(alpha[nx][ny]) - ord('A'))
+            if not (mask & c_bit):
+                stack.append((nx, ny, mask | c_bit, total + 1))
+
+print(answer)
 # ---------------------------------------
 # import sys
 # input = sys.stdin.readline
