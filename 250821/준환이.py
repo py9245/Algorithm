ca730feb72fddcb6ba329c634ee411d51e3c4eb8
py9245 <py9245@naver.com>
2025-08-22 09:15:52 +0900

    update

--- 250821/준환이.py
+++ 250821/준환이.py
@@ -2,14 +2,43 @@ import sys
 
 sys.stdin = open("input.txt", "r")
 
-T = int(input())
+input = sys.stdin.readline
 
+T = int(input())
 for case in range(1, T + 1):
     N = int(input())
-    nums = [list(map(int, input())) for _ in range(N)]
-    rem = N // 2
-    ans = 0
-    for i in range(N):
-        plus = rem - abs(rem - i)
-        ans += sum(nums[i][rem - plus: rem + plus + 1])
-    print(f"#{case} {ans}")
+    nums = sorted(map(int, input().split()), reverse=True)
+    total = sum(nums)
+
+    if N <= 2:
+        print(f"#{case} 1")
+        continue
+
+    fact = [1] * (N + 1)
+    pow2 = [1] * (N + 1)
+    for i in range(1, N + 1):
+        fact[i] = fact[i - 1] * i
+        pow2[i] = pow2[i - 1] * 2
+
+    ALL = (1 << N) - 1
+
+    def dfs(used, diff, remain, placed):
+        if used == ALL:
+            return 1
+        left_cnt = N - placed
+        if diff >= remain:
+            return fact[left_cnt] * pow2[left_cnt]
+
+        cnt = 0
+        for i in range(N):
+            if (used >> i) & 1:
+                continue
+            w = nums[i]
+            nu = used | (1 << i)
+            cnt += dfs(nu, diff + w, remain - w, placed + 1)
+            if diff >= w:
+                cnt += dfs(nu, diff - w, remain - w, placed + 1)
+        return cnt
+
+    answer = dfs(0, 0, total, 0)
+    print(f"#{case} {answer}")
