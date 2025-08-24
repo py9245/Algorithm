440226b30e6fd20a648a97b06dd5c04057933679
py9245 <py9245@naver.com>
2025-08-24 23:11:16 +0900

    update

---
+++ B_type/마라톤 코스/solution.py
@@ -0,0 +1,19 @@
+#####solution.py
+
+def init(N):
+    global n, node
+    n = N
+    node = [[] for _ in range(N + 1)]
+
+
+
+def addRoad(K, mID, mSpotA, mSpotB, mLen):
+    pass
+
+
+def removeRoad(mID):
+    pass
+
+
+def getLength(mSpot) -> int:
+    return 0
