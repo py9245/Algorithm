74fce8d7391891f429788f697d3ef5550dbb8c5c
py9245 <py9245@naver.com>
2025-08-25 16:01:38 +0900

    update

--- B_type/마라톤 코스/solution.py
+++ B_type/마라톤 코스/solution.py
@@ -1,14 +1,21 @@
 #####solution.py
 
 def init(N):
-    global n, node
+    global n, node, tree, tree_check, tree_cnt
     n = N
     node = [[] for _ in range(N + 1)]
-
-
+    tree = {}
+    tree_check = {}
+    tree_cnt = 0
 
 def addRoad(K, mID, mSpotA, mSpotB, mLen):
-    pass
+    tree_cnt += K
+    for _ in range(K):
+        node[mSpotA].append(mID)
+        node[mSpotB].append(mID)
+        tree[mID] = (mSpotA, mSpotB, mLen)
+        tree_check[mID] = True
+
 
 
 def removeRoad(mID):
