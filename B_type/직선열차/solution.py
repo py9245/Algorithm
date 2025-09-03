3aaf10650a12bf05f8220e25a5e400646c59af0e
py9245 <py9245@naver.com>
2025-09-03 17:55:37 +0900

    update

---
+++ B_type/직선열차/solution.py
@@ -0,0 +1,20 @@
+
+def init(N, K, mId, sId, eId, mInterval):
+	return
+
+
+def add(mId, sId, eId, mInterval):
+	return
+
+
+def remove(mId):
+	return
+
+
+def calculate(sId, eId):
+	return 0
+
+
+# 열차 1 ~ N 아이디를 가짐
+# 시작, 종착, 정차 간격
+# 출발에서 종착으로 가는 필요한 환승 횟수
