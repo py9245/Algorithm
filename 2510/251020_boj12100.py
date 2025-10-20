47b7d4440ea1eff2ea5e096a15a939e22dd6cd2b
yusin <py9245@naver.com>
2025-10-20 16:09:13 +0900

    update

---
+++ 2510/251020_boj12100.py
@@ -0,0 +1,71 @@
+import sys
+sys.stdin = open("251020_boj12100.txt", "r")
+
+from collections import deque
+
+MAX_CNT = 5
+TURN = 4
+
+def gravity_2048(cnt, cbd, col, b_last, last):
+    vals = []
+    for r in range(N):
+        cell = cbd[r][col]
+        if cell:
+            vals.append(cell[0])
+
+    merged = []
+    i = len(vals) - 1
+    while i >= 0:
+        if i > 0 and vals[i] == vals[i-1]:
+            merged.append(vals[i] * 2)
+            i -= 2
+        else:
+            merged.append(vals[i])
+            i -= 1
+
+    r = N - 1
+    for v in merged:
+        cbd[r][col] = [v] + [False] * MAX_CNT
+        r -= 1
+    while r >= 0:
+        cbd[r][col] = 0
+        r -= 1
+
+def deep_copy(bd):
+    return [[(cell[:] if isinstance(cell, list) else 0) for cell in row] for row in bd]
+
+def rotate90(bd):
+
+    rot = list(zip(*bd[::-1]))
+    return [[(cell[:] if isinstance(cell, list) else 0) for cell in row] for row in rot]
+
+def dfs(cnt, bd):
+    if cnt > MAX_CNT:
+        num = 0
+        for i in range(N):
+            for j in range(N):
+                if bd[i][j]:
+                    num = max(num, bd[i][j][0])
+        answer[0] = max(answer[0], num)
+        return
+    
+    cur = deep_copy(bd)
+    for _ in range(TURN):
+        c_bd = deep_copy(cur)
+        for c in range(N):
+            gravity_2048(cnt, c_bd, c, N - 2, N - 1)
+        dfs(cnt + 1, c_bd)
+        cur = rotate90(cur)
+
+N = int(input())
+dxy = [(N - 1, -1, -1),(1,0),(0,-1),(-1,0)]
+board = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        if board[i][j]:
+            board[i][j] = [board[i][j]] + [False] * 5
+
+answer = [0]
+dfs(1, board)
+print(answer[0])
--- 2510/251020_boj13460.py
+++ 2510/251020_boj13460.py
@@ -1,75 +1,77 @@
 import sys
-
 sys.stdin = open("251020_boj13460.txt", "r")
 
-import heapq
+from collections import deque
 
-dxy = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+dxy = [(0,1),(1,0),(0,-1),(-1,0)]
 N, M = map(int, input().split())
 board = [list(input()) for _ in range(N)]
 
 rx = ry = bx = by = ex = ey = 0
-
-for i in range(1, N - 1):
-    for j in range(1, M - 1):
+for i in range(N):
+    for j in range(M):
         if board[i][j] == "R":
             rx, ry = i, j
+            board[i][j] = '.'
         elif board[i][j] == "B":
             bx, by = i, j
+            board[i][j] = '.'
         elif board[i][j] == "O":
             ex, ey = i, j
 
-dist = [[float('inf')] * M for _ in range(N)]
-hq = []
+visited = [[[[False]*M for _ in range(N)] for _ in range(M)] for _ in range(N)]
 
-heapq.heappush(hq, (0, rx, ry, bx, by, [i[:] for i in board]))
+def roll(x, y, dx, dy):
+    cnt = 0
+    while True:
+        nx, ny = x + dx, y + dy
+        if board[nx][ny] == '#':
+            return x, y, cnt, False
+        x, y = nx, ny
+        cnt += 1
+        if (x, y) == (ex, ey):
+            return x, y, cnt, True
 
-dist[rx][ry] = 0
+dq = deque()
+dq.append((0, rx, ry, bx, by))
+visited[rx][ry][bx][by] = True
+bp = False
+while dq:
+    if bp:
+        break
+    cnt, crx, cry, cbx, cby = dq.popleft()
 
-while hq:
-    cnt, crx, cry, cbx, cby, visi = heapq.heappop(hq)
-    print(crx, cry)
-    if cnt > dist[rx][ry]:
+
+    if (cbx, cby) == (ex, ey):
         continue
-    
-    if cbx == ex and cby == ey:
-        continue
 
-    if crx == ex and cry == ey:
+    if (crx, cry) == (ex, ey):
+        bp = True
         print(cnt)
         break
-    
-    cnt += 1
+
+    if cnt >= 10:
+        break
 
     for dx, dy in dxy:
-        red_continue = True
-        blue_continue = True
-        move_rx, move_ry, move_bx, move_by = crx, cry, cbx, cby
-        new_visi = [i[:] for i in visi]
-        while red_continue or blue_continue:
-            if red_continue:
-                n_move_rx, n_move_ry = move_rx + dx, move_ry + dy
-                if new_visi[n_move_rx][n_move_ry] == "." or new_visi[n_move_rx][n_move_ry] == "O":
-                    new_visi[move_rx][move_ry] = "."
-                    new_visi[n_move_rx][n_move_ry] = "R"
-                    move_rx, move_ry = n_move_rx, n_move_ry
-                else:
-                    red_continue = False
-                if move_rx == ex and move_ry == ey:
-                    red_continue = False
-            if blue_continue:
-                n_move_bx, n_move_by = move_bx + dx, move_by + dy
-                if new_visi[n_move_bx][n_move_by] == "." or new_visi[n_move_bx][n_move_by] == "O":
-                    new_visi[move_bx][move_by] = "."
-                    new_visi[n_move_bx][n_move_by] = "B"
-                    move_bx, move_by = n_move_bx, n_move_by
-                else:
-                    blue_continue = False
-                if move_bx == ex and move_by == ey:
-                    blue_continue = False
-        
-        if dist[move_rx][move_ry] > cnt:
-            dist[move_rx][move_ry] = cnt
-            heapq.heappush(hq, [cnt, move_rx, move_ry, move_bx, move_by, [i[:] for i in new_visi]])
-else:
+        nrx, nry, rc, r_hole = roll(crx, cry, dx, dy)
+        nbx, nby, bc, b_hole = roll(cbx, cby, dx, dy)
+
+        if b_hole:
+            continue
+        if r_hole:
+            print(cnt + 1)
+            bp = True
+            break
+
+        if (nrx, nry) == (nbx, nby):
+            if rc > bc:
+                nrx -= dx; nry -= dy
+            else:
+                nbx -= dx; nby -= dy
+
+        if not visited[nrx][nry][nbx][nby]:
+            visited[nrx][nry][nbx][nby] = True
+            dq.append((cnt + 1, nrx, nry, nbx, nby))
+if not bp:
     print(-1)
