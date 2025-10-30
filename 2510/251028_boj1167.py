e07a2e3fc4670207a79f31612b28510ab0db0d51
yusin <py9245@naver.com>
2025-10-30 09:31:50 +0900

    upoet

--- 2510/251028_boj1167.py
+++ 2510/251028_boj1167.py
@@ -1,12 +1,9 @@
 import sys
-sys.stdin = open("251020_boj1167.txt", "r")
-
-
+sys.stdin = open("251028_boj1167.txt", "r")
 
 import sys, heapq
 
 input = sys.stdin.readline
-
 
 def find(x):
     if parent[x] != x:
@@ -15,18 +12,19 @@ def find(x):
 
 def uinon(a, b):
     a, b = find(a), find(b)
+    if a > b:
+        a, b = b, a
     if a != b:
         parent[b] = a
 
 
 N = int(input())
-nodes = []
+nodes = [[] for _ in range(N + 1)]
 parent = [i for i in range(N + 1)]
 for i in range(1, N + 1):
     nums = list(map(int, input().split()))
     idx = nums[0]
     nums_len = len(nums)
     for i in range(nums_len//2 - 1):
-        heapq.heappush(nodes, [-(nums[i * 2 + 2]), idx, nums[i * 2 + 1]])
+        nodes[idx].append([-(nums[i * 2 + 2]), nums[i * 2 + 1]])
 print(nodes)
-
