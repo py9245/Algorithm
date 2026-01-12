8ba05bc0745baa3c8fd403bc2a5b4c5a95ed581c
oriwacat <ksgg3911@gmail.com>
2026-01-12 17:34:46 +0900

    update

--- 2601/사람네트워크.py
+++ 2601/사람네트워크.py
@@ -1,6 +1,10 @@
 import sys
 
-sys.stdin = open("input.txt", "r")
+sys.stdin = open("input2.txt", "r")
+
+from collections import deque
+
+INF = 10 ** 9
 
 T = int(input())
 
@@ -11,3 +15,34 @@ for tc in range(1, T + 1):
 
     for i in range(N):
         for j in range(N):
+            used = arr[i * N + j + 1]
+            if used : node[i].append(j)
+    
+    answer = INF
+
+    for n in range(N):
+
+        dist = [INF] * N
+        queue = deque()
+        queue.append([n, 0])
+        dist[n] = 0
+        current_min_total = 0
+        while queue:
+            if current_min_total >= answer:
+                break
+            c_node, cnt = queue.popleft()
+            n_cnt = cnt + 1
+            for n_node in node[c_node]:
+                if current_min_total >= answer:
+                    break
+
+                if dist[n_node] <= n_cnt:
+                    continue
+                
+                current_min_total += n_cnt
+                dist[n_node] = n_cnt
+                queue.append([n_node, n_cnt])
+        
+        answer = min(answer, sum(i for i in dist if i != INF))
+
+    print(f"#{tc} {answer}")
---
+++ 2601/화학물질.py
@@ -0,0 +1,42 @@
+import sys
+
+sys.stdin = open("input2.txt", "r")
+
+from collections import deque
+
+T = int(input())
+
+dxy = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    
+    cluster = []
+
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 0:
+                continue
+            
+            arr[i][j] = 0
+            min_x, min_y, max_x, max_y = i, j, i, j
+            queue = deque()
+            queue.append((i, j))
+
+            while queue:
+                x, y = queue.popleft()
+
+                for dx, dy in dxy:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] != 0:
+                        arr[nx][ny] = 0
+                        queue.append((nx, ny))
+                        min_x = min(min_x, nx)
+                        max_x = max(max_x, nx)
+                        min_y = min(min_y, ny)
+                        max_y = max(max_y, ny)
+            cluster.append([max_x- min_x + 1, max_y - min_y + 1])
+    
+    print(cluster)
+
