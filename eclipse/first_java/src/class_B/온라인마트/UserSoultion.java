68113c9a061655f7380ef3c5b636a14edd997aab
py9245 <py9245@naver.com>
2026-01-25 12:00:00 +0900

    update

--- eclipse/first_java/src/class_B/온라인마트/UserSoultion.java
+++ eclipse/first_java/src/class_B/온라인마트/UserSoultion.java
@@ -1,5 +1,31 @@
 package class_B.온라인마트;
 
-public class UserSoultion {
+class UserSolution
+{
+    public void init()
+    {
+        return;
+    }
 
+    public int sell(int mID, int mCategory, int mCompany, int mPrice)
+    {
+        return -1;
+    }
+
+    public int closeSale(int mID)
+    {
+        return -1;
+    }
+
+    public int discount(int mCategory, int mCompany, int mAmount)
+    {
+        return -1;
+    }
+
+    Solution.RESULT show(int mHow, int mCode)
+    {
+        Solution.RESULT res = new Solution.RESULT();
+
+        return res;
+    }
 }
