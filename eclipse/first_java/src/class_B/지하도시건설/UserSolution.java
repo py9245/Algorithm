600047d9cc4784fb00b99f6f8647d03b6e0318bf
py9245 <py9245@naver.com>
2026-02-19 23:08:38 +0900

    update

---
+++ eclipse/first_java/src/class_B/지하도시건설/UserSolution.java
@@ -0,0 +1,26 @@
+package class_B.지하도시건설;
+
+class UserSolution {
+
+	
+//	DropBox는 최대 10_000회
+//	explore 최대 10회 1만개의 간선을 정점을 타는건 쉬울듯
+//	높이 최대 1000개 만큼의 arraylist를 만듬
+	
+	static int h, w;
+	
+	public void init(int mH, int mW) {
+		h = mH;
+		w = mW;
+		
+	}
+
+	public int dropBox(int mId, int mLen, int mExitA, int mExitB, int mCol) {
+		return 0;
+	}
+
+	public int explore(int mIdA, int mIdB) {
+		return -1;
+	}
+
+}
